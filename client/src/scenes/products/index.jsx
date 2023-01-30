63ab5b39f79ca3e0b7743bb9beb1c086ae7fec3d
Felipe Marques <felipe.marqs@outlook.com>
2023-01-30 19:35:10 -0300

    att

---
+++ client/src/scenes/products/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import Header from "components/Header";
+import { useGetProductsQuery } from "state/api";
+
+const Products = () => {
+  return (
+    <Box>
+      <Header title="PRODUCTS" subtitle="Products Details" />
+    </Box>
+  );
+};
+
+export default Products;
