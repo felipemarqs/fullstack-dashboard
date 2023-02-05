a23d2ea3f7b23eb94ea55aefcde5b9345d3c0ffe
Felipe Marques <felipe.marqs@outlook.com>
2023-02-05 11:14:30 -0300

    feat: :sparkles: Admins Page
    Admins Page Created using MUI DataGrid

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ import Geography from 'scenes/geography'
 import Overview from 'scenes/overview'
 import Daily from 'scenes/daily'
 import Monthly from 'scenes/monthly'
-
+import Breakdown from 'scenes/breakdown'
+import Admin from 'scenes/admin'
 
 function App() {
 
@@ -38,7 +39,8 @@ function App() {
                     <Route path='/overview' element={<Overview/>}/>
                     <Route path='/daily' element={<Daily/>}/>         
                     <Route path='/monthly' element={<Monthly/>}/>
-                    <Route path='/monthly' element={<Monthly/>}/>
+                    <Route path='/breakdown' element={<Breakdown/>}/>
+                    <Route path='/admin' element={<Admin/>}/>
                 </Route>
              </Routes>
           </ThemeProvider>
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Costumers", "Transactions", "Geography", "Sales"],
+  tagTypes: [
+    "User",
+    "Products",
+    "Costumers",
+    "Transactions",
+    "Geography",
+    "Sales",
+    "Admins",
+  ],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -26,13 +34,17 @@ export const api = createApi({
       providesTags: ["Transactions"],
     }),
     getGeography: build.query({
-      query: () => 'client/geography',
-      providesTags: ["Geography"]
+      query: () => "client/geography",
+      providesTags: ["Geography"],
     }),
     getSales: build.query({
       query: () => "sales/sales",
-      providesTags: ["Sales"]
-    })
+      providesTags: ["Sales"],
+    }),
+    getAdmins: build.query({
+      query: () => "management/admins",
+      providesTags: ["Admins"],
+    }),
   }),
 });
 
@@ -42,5 +54,6 @@ export const {
   useGetCostumersQuery,
   useGetTransactionsQuery,
   useGetGeographyQuery,
-  useGetSalesQuery
+  useGetSalesQuery,
+  useGetAdminsQuery
 } = api;
